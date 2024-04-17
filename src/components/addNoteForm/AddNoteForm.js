aec5c52c2d58a6a49bcc7d8c682c4423019482a8
developer-tvc <developer@techversantinfo.com>
2024-04-17 17:19:55 +0530

    change in ui

--- src/components/addNoteForm/AddNoteForm.js
+++ src/components/addNoteForm/AddNoteForm.js
@@ -1,46 +1,58 @@
 import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 
-function AddNoteForm({ onAddNote }) {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+function AddNoteForm({ onAddNote, closeModal }) {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Check if title and content are not empty
-    if (!title.trim() || !content.trim()) return;
-    // Call the onAddNote function passed from the parent component
-    onAddNote({ title, content });
-    // Clear the input fields after adding the note
-    setTitle('');
-    setContent('');
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Check if title and content are not empty
+        if (!title.trim() || !content.trim()) return;
+        // Call the onAddNote function passed from the parent component
+        onAddNote({ title, content });
+        // Clear the input fields after adding the note
+        setTitle('');
+        setContent('');
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className=" max-w-[800px] mt-8 w-full ">
-      <div className="mb-4">
-        <label htmlFor="title" className="block text-gray-700 font-bold mb-2">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="content" className="block text-gray-700 font-bold mb-2">Content:</label>
-        <textarea
-          id="content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        ></textarea>
-      </div>
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-        Add Note
-      </button>
-    </form>
-  );
+    const onClose=()=>{
+        setTitle('');
+        setContent('');
+        closeModal()
+    }
+    return (
+        <form onSubmit={handleSubmit} className="p-12 rounded bg-white mx-auto w-1/2 h-[80%] mt-12 relative ">
+            <div onClick={onClose} className="h-6 w-6 absolute top-[10px] right-[10px] cursor-pointer">
+                <CloseIcon />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="title" className="block text-gray-700 font-bold mb-2">Title:</label>
+                <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="content" className="block text-gray-700 font-bold mb-2">Content:</label>
+                <textarea
+                    id="content"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-h-[50vh]"
+                ></textarea>
+            </div>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Add Note
+            </button>
+            <button onClick={onClose} className="bg-[tomato] ml-2 hover:bg-[#db5959] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Cancel
+            </button>
+        </form>
+    );
 }
 
 export default AddNoteForm;
--- src/components/notesListView/NotesListView.js
+++ src/components/notesListView/NotesListView.js
@@ -1,44 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import AddNoteForm from '../addNoteForm/AddNoteForm';
-import NoteCard from '../noteCard/NoteCard';
-import ModalView from '../modalView/ModalView';
+import React, { useEffect, useState } from 'react'
+import AddNoteForm from '../addNoteForm/AddNoteForm'
+import NoteCard from '../noteCard/NoteCard'
+import ModalView from '../modalView/ModalView'
+import { Modal } from '@mui/material'
 
 function NotesListView() {
-  const [notes, setNotes] = useState([]);
-  const [selectedNote, setSelectedNote] = useState();
-  const [isModalVisible, setIsModalVisible] = useState(false);
+    const [notes, setNotes] = useState([])
+    const [selectedNote, setSelectedNote] = useState()
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const [isFormVisible, setIsFormVisible] = useState(false)
 
-  const handleAddNote = (newNote) => {
-    // Add the new note to the existing array of notes
-    const noteId = Date.now();
-    const noteWithId = { id: noteId, ...newNote };
-    setNotes([...notes, noteWithId]);
-  };
+    const handleAddNote = newNote => {
+        // Add the new note to the existing array of notes
+        const noteId = Date.now()
+        const noteWithId = { id: noteId, ...newNote }
+        setNotes([...notes, noteWithId])
+        setIsFormVisible(false)
+    }
 
-  const onDelete =()=>{}
-  const onEdit =()=>{}
-  const onView =(note)=>{
-    setSelectedNote(note)
-    setIsModalVisible(true)
-  }
+    const onDelete = () => { }
+    const onEdit = () => { }
+    const onView = note => {
+        setSelectedNote(note)
+        setIsModalVisible(true)
+    }
 
-  const onModalClose =()=>{
-    setIsModalVisible(false)
-    setSelectedNote(null)
-    
-  }
+    const onModalClose = () => {
+        setIsModalVisible(false)
+        setSelectedNote(null)
+    }
 
-  return (
-    <div className='flex flex-col w-full items-start'>
-      <AddNoteForm onAddNote={handleAddNote} />
-      <div className='mt-4 flex flex-wrap gap-2' >
-        {notes.map((note, index) => (
-         <NoteCard key={index}  note={note} onDelete={onDelete} onEdit={onEdit} onView={onView}/>
-        ))}
-      </div>
-      <ModalView isVisibile={isModalVisible} note={selectedNote} onModalClose={onModalClose}/>
-    </div>
-  );
+    const onFormClose = () => { }
+
+    return (
+        <div className='flex flex-col w-full items-start'>
+            <div className="flex justify-end w-full">
+                <div onClick={() => setIsFormVisible(true)} className="bg-blue-500 cursor-pointer hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                    Add Note
+                </div>
+            </div>
+            <div className="fixed bottom-[100px]  right-[60px]">
+                <p className="text-sm">Total Notes : {notes.length} </p>
+            </div>
+          {notes.length > 0 ?   <div className='mt-4 flex flex-wrap gap-2'>
+                {notes.map((note, index) => (
+                    <NoteCard
+                        key={index}
+                        note={note}
+                        onDelete={onDelete}
+                        onEdit={onEdit}
+                        onView={onView}
+                    />
+                ))}
+            </div>: <div className='grid place-content-center h-[60vh] w-full text-2xl'>
+                No notes found
+                </div>}
+            <ModalView
+                isVisibile={isModalVisible}
+                note={selectedNote}
+                onModalClose={onModalClose}
+            />
+            <Modal open={isFormVisible} onClose={onFormClose}>
+                <AddNoteForm onAddNote={handleAddNote} closeModal={() => setIsFormVisible(false)} />
+            </Modal>
+        </div>
+    )
 }
 
-export default NotesListView;
+export default NotesListView
